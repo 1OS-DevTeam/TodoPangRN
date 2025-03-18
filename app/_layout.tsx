23530f9aef0304d5b9ec44030cb6165e746f6a72
sungyeonkim <wangkobong@gmail.com>
2025-03-18 16:15:57 +0900

    feat - 파이어베이스 config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Stack } from 'expo-router';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../firebaseConfig';
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+console.log('Firebase 초기화 완료:', app);
 
 const AppLayout = () => {
   return (
